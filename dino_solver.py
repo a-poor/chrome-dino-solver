084435ff0a749d355ca987cc38764af70d909c34
a-poor <a-poor@users.noreply.github.com>
2020-03-10 18:12:09 -0400

    Still not worknig great but set up to log and save

--- dino_solver.py
+++ dino_solver.py
@@ -1,3 +1,27 @@
+"""
+dino_solver.py
+
+by Austin Poor
+
+
+
+NOTES:
+– Run for longer to see how it changes
+– save the memory to a SQLite DB so that it doesn't
+  need to re-learn everything every time
+– give it a past frame in addition to current (or more)
+– can you scale the inputs? (how would you handle)
+– use more/less layers? to improve training?
+– add in random actions (epsilon-greedy)
+
+
+– run multiple training sessions simultaneously
+– run training headless in the cloud?
+
+
+"""
+
+
 
 import time
 from collections import deque
@@ -7,16 +31,15 @@ from selenium.webdriver.common.keys import Keys
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model, save_model
 from tensorflow.keras.layers import Dense, Dropout
 
 URL = 'chrome://dino/'
 CSS_RUNNER_CONTAINER = '.runner-container'
 
-# driver = webdriver.Chrome('./chromedriver')
-# driver.get(URL)
-
+dt_fmt = time.strftime("%Y%m%d.%H%M%S")
 
 class DinoGame:
     URL = 'chrome://dino/'
@@ -61,6 +84,11 @@ class DinoGame:
     INPUT_DIM = 8
     ACTION_DIM = 3
 
+    @classmethod
+    def load_brain(cls,model_path):
+        return cls.__init__(load_model(model_path))
+
+
     def __init__(self,brain=None):
         self.driver = webdriver.Chrome('./chromedriver')
         self.driver.get(self.URL)
@@ -97,10 +125,10 @@ class DinoGame:
             activation="relu"
         ))
         m.add(Dropout(0.2))
-        m.add(Dense(
-            16,
-            activation="relu"
-        ))
+        # m.add(Dense(
+        #     16,
+        #     activation="relu"
+        # ))
         m.add(Dense(
             self.ACTION_DIM,
             activation='linear'
@@ -138,11 +166,14 @@ class DinoGame:
 
     #### RL Functions ####
 
-    def memorize(self,state_info,action):
+    def memorize(self,state_info,action,reward=1):
         self.memory.append(np.concatenate((
             state_info,
-            (action,)
-            )))
+            (
+                action,
+                reward
+            )
+        )))
         return
 
     def get_action(self,state):
@@ -161,7 +192,7 @@ class DinoGame:
                 s = self.memory[i]
                 # Unpack the data
                 (
-                    crashed,
+                    _,
                     _,
                     tRexX,
                     tRexY,
@@ -171,7 +202,8 @@ class DinoGame:
                     obs2Y,
                     obs3X,
                     obs3Y,
-                    action
+                    action,
+                    reward
                 ) = s
                 # Get the brain's current prediction
                 model_in = np.array([
@@ -185,17 +217,16 @@ class DinoGame:
                     obs3Y
                 ])
                 pred = self.brain.predict(model_in)[0]
-                # print("[176] prediction shape:", pred.shape)
 
                 # NOTE: Add extra reward if not done
                 # and obstacle behind player
-                reward = 1
-                if crashed:
-                    reward = -5
-                else: 
-                    # NOTE: This is where we'll add
-                    # the predicted future reward
-                    reward += 0.9
+                # reward = 1
+                # if crashed:
+                #     reward = -5
+                # else: 
+                #     # NOTE: This is where we'll add
+                #     # the predicted future reward
+                #     reward += 0.9
                 
                 # Update the reward
                 pred[action] = reward
@@ -211,8 +242,10 @@ class DinoGame:
     #### Run Training Session ####
 
     def train(self,n_episodes=5):
+        final_dists = []
         for episode in range(n_episodes):
             self.move_jump()
+            started = False
             time.sleep(1)
             done = False
             while not done:
@@ -230,6 +263,13 @@ class DinoGame:
                     obs3X,
                     obs3Y
                 ) = s
+                if done and not started:
+                    done = False
+                    self.move_jump()
+                    time.sleep(0.5)
+                    continue
+                elif not started:
+                    started = True
                 
                 # Choose an action
                 state = np.array((
@@ -248,12 +288,33 @@ class DinoGame:
                 # Make the move
                 self.move(action)
 
+                # calculate reward
+                reward = 1
+
                 # store that information
-                self.memorize(s,action)
+                self.memorize(s,action,reward)
 
                 # Take a lil break
-                time.sleep(0.05)
+                time.sleep(0.1) # NOTE: adjust this time?
+            final_dists.append(dist)
+            
+            CRASH_BACWARD_DEPTH = 30
+            CRASH_REWARD_DECAY = 0.85
+            CRASH_REWARD = -5
+            REWARD_MEMORY_POS = -1
+            for i in range(CRASH_BACWARD_DEPTH):
+                self.memory[-(1+i)][REWARD_MEMORY_POS] += (
+                    CRASH_REWARD * CRASH_REWARD_DECAY ** i
+                )
             print(f"EPISODE: {episode:3d} | DISTANCE RAN: {dist:20.2f}")
+        return final_dists
+
+    
+    def save_brain(self,filepath):
+        save_model(
+            self.brain,
+            filepath
+            )
 
 
     def play(self):
@@ -263,12 +324,13 @@ class DinoGame:
 
     
 if __name__ == "__main__":
+    dt_fmt = time.strftime("%Y%m%d.%H%M%S")
     print("Program starting")
     print("building dino...")
     try:
         runner = DinoGame()
         print("Starting game")
-        runner.train(200)
+        dists = runner.train(20)
     finally:
         runner.driver.close()
         print("Done")
@@ -286,9 +348,23 @@ if __name__ == "__main__":
             "obs2Y",
             "obs3X",
             "obs3Y",
-            "action"
+            "action",
+            "reward"
         ]
     )
+
+    runner.save_brain(f"models/brain_{dt_fmt}.h5")
+
+    with open(f"logs/log_{dt_fmt}.txt",'w') as f:
+        f.write(f"Best dist: {df.dist.max()}\n")
+        f.write(f"Avg reward: {df.reward.mean()}\n")
+        f.write("\n\nAction breakdown:\n")
+        f.write(str(df.action.value_counts()))
+
+    plt.plot(dists)
+    plt.title("tRex Distance Traveled")
+    plt.savefig(f"./images/trex_dist_plot{dt_fmt}.png")
+
 
 
 
